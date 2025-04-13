f1b16f7198d991669dd2286b9bbc58f8995da258
Ramalingam S <codeandgyan@gmail.com>
2025-04-14 01:02:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Viber-IDE for Vibe Coding
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const express = require("express");
+const { Server: SocketServer } = require("socket.io");
+const { spawn } = require("child_process");
+const os = require("os");
+
+const osShell = os.platform() === "win32" ? "powershell.exe" : "bash";
+
+const shell = spawn(osShell, [], {
+  // Start a persistent bash session
+  cwd: process.env.INIT_CWD,
+});
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer({
+  cors: "*",
+});
+
+io.attach(server);
+
+shell.stdout.on("data", (data) => {
+  console.log("emitting term data:", data.toString());
+  io.emit("terminal:data", data.toString());
+});
+
+shell.stderr.on("data", (data) => {
+  // console.error(`stderr: ${data.toString()}`);
+  io.emit("terminal:data", data.toString());
+});
+
+shell.on("close", (code) => {
+  console.log(`Shell process exited with code ${code}`);
+});
+
+server.listen(9000, () => {
+  console.log("🐳 Docker Server is running on port 9000");
+});
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on("terminal:write", (data) => {
+    console.log("received an event", data);
+    shell.stdin.write(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected: ${socket.id}`);
+  });
+});
